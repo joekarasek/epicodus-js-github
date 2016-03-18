8a07c3938ed5a78c75a2d8a7465c2cd535f37a28
Joe Karasek <joseph.karasek@gmail.com>
2016-03-18 09:26:37 -0700

    print to console object with structured data to complete assignment

--- js/getRepos.js
+++ js/getRepos.js
@@ -3,11 +3,22 @@ var apiKey = require('./../.env').apiKeyGithub;
 
 // function getRepos: Takes a user name string and returns a user object with the user name and a list array of user's public repos (repo name and description)
 exports.getRepos = function(userName){
-  var userReposObject = {
-    name: userName
+  // Return variable to populate with repo data
+  var userReponseObject = {
+    name: userName,
+    repos: []
   };
+  // Ajax request to get all repo information for userName
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    //Sort through response to take out unneeded information, pushes repo object to userReponseObject array of repos
+    response.forEach(function(repo) {
+      var miniRepo = {
+        name: repo.name,
+        description: repo.description
+      };
+      userReponseObject.repos.push(miniRepo);
+    });
+    console.log(userReponseObject);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
