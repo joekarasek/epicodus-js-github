7b5849bb637a57f5c9b3267620a370c82373ac8b
Joe Karasek <joseph.karasek@gmail.com>
2016-03-18 10:31:30 -0700

    add UI function to create DOM list of repos as a string

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,5 +1,14 @@
 var getRepos = require('./../js/getRepos.js').getRepos;
 
+var populateRepoList = function(userRepos) {
+  var response = '';
+  userRepos.repos.forEach(function(repo) {
+
+    response += '<li><div class="collapsible-header">' + repo.name + '</div><div class="collapsible-body"><p>Description: ' + repo.description + '</p></div></li>';
+  });
+  return reponse;
+};
+
 $(document).ready(function(){
 
   // On submit of userName form
