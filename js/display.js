b4f70d3cbd5fb4ed09fbee84326f0d4ec25271f6
Joe Karasek <joseph.karasek@gmail.com>
2016-03-18 20:58:08 -0700

    add custom styling to display of user info and gravitar

--- js/display.js
+++ js/display.js
@@ -9,8 +9,9 @@ exports.listRepos = function(response) {
 
 exports.showUserInfo = function(response) {
   console.log(response);
-  // $('#repo_list').empty();
-  // response.forEach(function(repo) {
-  //   $('#repo_list').append('<li><div class="collapsible-header">' + repo.name + '</div><div class="collapsible-body"><p>Description: ' + repo.description + '</p></div></li>');
-  // });
+  $('.user-wrapper').empty();
+  $('.user-wrapper')
+    .append('<img class="z-depth-2" src=' + response.avatar_url + ' alt="gitHub gravitar user"/>')
+    .append('<p class="user-header">User ID:</p><p class="user-info">' + response.login + '</p>');
+  if (response.email) $('.user-wrapper').append('<p class="user-header">User Email:</p><p class="user-info">' + response.email + '</p>');
 };
